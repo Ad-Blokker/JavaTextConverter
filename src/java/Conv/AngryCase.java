2033b945f5c5d5a4f66cd77f4831f9cfac6a494a
MrYoranimo <yvulker@gmail.com>
2022-11-11 00:45:16 +0100

    Introduce realAngryCase method in AngryCase class
    Strings passed onto the method will become upper-cased. A period will be
    placed after every word, which is determined based on separation by a white
    space character.

--- src/java/Conv/AngryCase.java
+++ src/java/Conv/AngryCase.java
@@ -11,7 +11,54 @@ public class AngryCase {
 
 	String output = "";
 
-	void angryCaseConv(String input) {
+    public static String realAngryCase(String input) {
+		// store result in builder
+		StringBuilder builder = new StringBuilder();
+
+		// shouldPlaceMarker indicates whether a period should be added or has already been added for the current
+		// whitespace
+		boolean shouldPlaceMarker = false;
+
+		// convert input to upper case, as all characters will otherwise be uppercased in the loop
+		input = input.toUpperCase();
+
+		// iterate over input characters
+		for (int i = 0; i < input.length(); i++) {
+			char current = input.charAt(i);
+
+			// check if current character is whitespace
+			if (Character.isWhitespace(current)) {
+				// append a period to the output if that has not been done already
+				if (shouldPlaceMarker) {
+					builder.append(".");
+					shouldPlaceMarker = false;
+				}
+
+				// append current whitespace to the output buffer
+				builder.append(current);
+
+				// continue with the next character
+				continue;
+			}
+
+			// append current character (non-whitespace) to the output buffer
+			builder.append(current);
+
+			// indicative to the next iteration that the previous character was not a whitespace character,
+			// so that a period may be appended to the output buffer if a whitespace character was found.
+			shouldPlaceMarker = true;
+		}
+
+		// end the sentence with a period.
+		if (shouldPlaceMarker) {
+			builder.append(".");
+		}
+
+		// return resulting output buffer
+		return builder.toString();
+    }
+
+    void angryCaseConv(String input) {
 
 		boolean lastCharDot = false;
 		output += input.toUpperCase().charAt(0);
